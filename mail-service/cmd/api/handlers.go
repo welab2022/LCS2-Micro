a14280e389b6f36973e069f6e19736a29d872c86
Vu Phan <vuphan@VUPHAN-PC.fritz.box>
2022-11-07 02:49:36 +0100

    fix uri

--- mail-service/cmd/api/handlers.go
+++ mail-service/cmd/api/handlers.go
@@ -29,6 +29,8 @@ func (app *Config) SendMail(w http.ResponseWriter, r *http.Request) {
 		Data:    requestPayload.Message,
 	}
 
+	log.Printf("mail: %s", msg)
+
 	err = app.Mailer.SendSMTPMessage(msg)
 	if err != nil {
 		log.Println(err)
